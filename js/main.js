b20cf97b8e44a04e24d5c5e4885becc93eb68546
Anderson Guedes <anderson.guedes@outlook.com.br>
2020-03-01 10:52:20 -0300

    Inicio da section de skills

--- js/main.js
+++ js/main.js
@@ -133,9 +133,34 @@ function scrollToTop() {
 	offsetTop = offset.top;
 	$('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
 }
+
 // =====================================================================================================
 // Tooltip
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+// =====================================================================================================
+// ProgressBar
+var lang = {
+  "html": "100%",
+  "css": "90%",
+  "javascript": "70%",
+  "php": "55%",
+  "angular": "65%"
+};
+
+var multiply = 4;
+
+$.each( lang, function( language, pourcent) {
+
+  var delay = 700;
+  
+  setTimeout(function() {
+    $('#'+language+'-pourcent').html(pourcent);
+  },delay*multiply);
+  
+  multiply++;
+
+});
 // =====================================================================================================
